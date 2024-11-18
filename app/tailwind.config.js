88ea43a183d0b81c39f98e2fdd12d9cfe5b2d320
m-wilk <magdalenawilk86@gmail.com>
2024-11-18 15:00:44 +0100

    finished Hero

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
     extend: {
       colors: {
         primary: "#e3ff04",
-        "black-90": "rgba(24, 24, 24, 0.1);"
+        "black-90": "rgba(24, 24, 24, 0.1);",
+        "white-15": "rgba(255, 255, 255, 0.15)"
 
       },
       backgroundImage: {
