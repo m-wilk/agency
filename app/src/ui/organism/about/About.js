fb66a8de808af6ad05b3cbd27d45df5f66652f7a
m-wilk <magdalenawilk86@gmail.com>
2024-11-19 14:03:11 +0100

    zmiany poprawione

--- app/src/ui/organism/about/About.js
+++ app/src/ui/organism/about/About.js
@@ -7,27 +7,27 @@ const About = () => {
       <div className="grid grid-rows-1  grid-cols-2 gap-4 container">
         <Image className="w-full" src={AboutImg} />
         <div className="ms-24">
-          <div className="flex items-center gap-3 text-primary border border-white rounded-full px-6 py-3 inline-flex mb-8">
-            <span className="uppercase">About Company</span>
+          <div className="flex items-center gap-3 text-primary border border-white rounded-full px-6 py-3 inline-flex mb-8 uppercase">
+            About Company
             <span className="icon-arrow-up-left2"></span>
           </div>
           <h2 className="text-white text-6xl font-bold leading-[125%] mb-8">
             We Create Impactful <br /> Digital Experiences
           </h2>
-          <div className="flex items-center justify-between mb-8">
+          <div className="flex items-center justify-between mb-8 text-white">
             <div className="text-white">
               <h2 className="text-4xl font-bold">25+</h2>
               <span className="text-gray-300">Completed projects</span>
             </div>
-            <p className="text-white">
+            <p>
               Bring to the table win-win survival strategies to ensure <br />
               proactive domination. At the end of the day, going
             </p>
           </div>
-          <div className="flex justify-between mb-8">
-            <h5 className="text-primary text-2xl font-bold">2019</h5>
-            <h5 className="text-primary text-2xl font-bold">2021</h5>
-            <h5 className="text-primary text-2xl font-bold">2024</h5>
+          <div className="flex justify-between mb-8 text-primary text-2xl font-bold">
+            <h5>2019</h5>
+            <h5>2021</h5>
+            <h5>2024</h5>
           </div>
           <div className="flex justify-between border-primary border-t pt-8 relative">
             <div className="before:content-[''] before:absolute before:w-0 before:h-0 before:border-l-8 before:border-r-8 before:border-t-8 before:border-l-transparent before:border-r-transparent before:border-t-primary before:top-0 before:left-0">
