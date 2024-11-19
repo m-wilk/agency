c4f280e892d7598aa8a728ad000555aaa4cfdbc0
m-wilk <magdalenawilk86@gmail.com>
2024-11-19 12:54:18 +0100

    about all

--- app/src/ui/pages/home/Home.js
+++ app/src/ui/pages/home/Home.js
@@ -1,11 +1,13 @@
 import Header from "../../organism/header/Header";
 import Hero from "../../organism/hero/Hero";
+import About from "../../organism/about/About";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Hero />
+      <About />
     </>
   );
 };
